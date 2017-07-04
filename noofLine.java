1ed0c4ec902044a5ffdc6b7f30f9ee28456a4178
Saipreetha <saipreethaekb@gmail.com>
2017-07-04 09:44:21 +0530

    Add files via upload

---
+++ noofLine.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.Scanner;
+public class noofLine {
+	public static void main(String[] args){
+		System.out.println("Enter the para");
+		Scanner scan=new Scanner(System.in);
+		String line=scan.nextLine();
+		int Count=0;
+		scan.close();
+		String[] sentenceList = line.split("[!?.:]+");
+        Count += sentenceList.length;
+		System.out.println(Count);
+	}
+
+}
+
