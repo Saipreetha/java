445e05603a254b73bef988e8d72220e3e2c76464
Saipreetha <saipreethaekb@gmail.com>
2017-07-04 14:24:55 +0530

    Add files via upload

---
+++ arrayNum.java
@@ -0,0 +1,23 @@
+package guvi;
+import java.util.Scanner;
+public class arrayNum {
+       public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+	   System.out.println("Enter the size:");
+       int size=sc.nextInt();
+       int []a=new int[size];
+	   System.out.println("The elements are");
+       for(int i=0;i<size;i++){
+    	   a[i]=sc.nextInt();
+       }
+	   System.out.println("Repeated Nums are:");
+       for(int i=0;i<size;i++){
+    	   for(int j=i+1;j<size;j++){
+    		   if(a[i]==a[j]){
+    			   System.out.println(a[i]);
+    		   }
+    	   }
+       }
+       sc.close();
+	}
+}
